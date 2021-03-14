6444f213a0ec1d1a20ecc5cf8be5ce1708893403
artkescha <asavarin1910@gmail.com>
2021-03-14 23:26:10 +0300

    fix package

--- check_solution/generate.go
+++ check_solution/generate.go
@@ -1,8 +1,3 @@
-package send_solution
+package check_solution
 
-// чтоб работало c gogoproto
-// go get github.com/gogo/protobuf/proto
-// go get github.com/gogo/protobuf/jsonpb
-// go get github.com/gogo/protobuf/protoc-gen-gogo
-// go get github.com/gogo/protobuf/gogoproto
 //go:generate protoc -I=. -I=$GOPATH/src/github.com/gogo/protobuf/protobuf -I=$GOPATH/src --micro_out=. --go_out=plugins=grpc:. check_solution.proto
