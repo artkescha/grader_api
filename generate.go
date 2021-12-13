d665b87407669d2e5502c66a59c973648202a3d3
artkescha <work.interin@yandex.ru>
2021-12-13 07:59:29 +0300

    regenerate api

--- generate.go
+++ generate.go
@@ -4,5 +4,6 @@ package grader_api
 ////go:generate protoc -I=. -I=$GOPATH/src/github.com/gogo/protobuf/protobuf -I=$GOPATH/src --micro_out=. --go_out=plugins=grpc:. check_solution.proto
 
 ////Working!
-///go:generate protoc --go_out=:. send_solution.proto
-///go:generate protoc --go-grpc_out=. check_solution.proto
+//go:generate protoc --go_out=:. send_solution.proto
+//go:generate protoc --go-grpc_out=. check_solution.proto
+//go:generate protoc --go_out=:. check_solution.proto
